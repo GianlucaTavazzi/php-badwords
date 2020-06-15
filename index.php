5609880b352ad333bb6d34b41352dc96aa5f4ef5
Gianluca Tavazzi <gianluca.tavazzi@gmail.com>
2020-06-15 21:23:58 +0200

    fine esercizio

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//questa è una variabile
+$string = 'Ciao sono Michele';
+$censured = $_GET['badword'];
+$partial_string = substr ( $censured , 0 , 3 );
+$sub_censured = '***';
+$final_sentence = str_replace ($partial_string, $sub_censured, $string);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <p> <?php echo $string ?> </p>
+        <p> <?php echo $censured ?> </p>
+        <p> <?php echo $partial_string ?> </p>
+        <p> <?php echo $final_sentence ?> </p>
+    </body>
+</html>
